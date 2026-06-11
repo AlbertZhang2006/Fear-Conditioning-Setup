5a2cb6c4865fa23f235e3fc80757f223d0f4836a
lukeoh830 <lukeoh830@gmail.com>
2026-06-11 11:14:49 -0400

    debug camera v1

--- shock/shock.ino
+++ shock/shock.ino
@@ -1,8 +1,12 @@
 const int shockPin = 3;
+const int cameraTriggerPin = 2;
 
 void setup() {
   pinMode(shockPin, OUTPUT);
   digitalWrite(shockPin, HIGH); // OFF (active-low)
+
+  pinMode(cameraTriggerPin, OUTPUT);
+  digitalWrite(cameraTriggerPin, LOW);
 
   Serial.begin(115200);
 }
@@ -18,6 +22,14 @@ void loop() {
 
     else if (cmd == "SHOCK_OFF") {
       digitalWrite(shockPin, HIGH);
+    }
+
+    else if (cmd == "CAMERA_ON") {
+      digitalWrite(cameraTriggerPin, HIGH);
+    }
+
+    else if (cmd == "CAMERA_OFF") {
+      digitalWrite(cameraTriggerPin, LOW);
     }
   }
 }
